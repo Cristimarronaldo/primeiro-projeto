1d9edd0b1ac3fa86a95b42e3ed03b9105bd864df
Cristimarronaldo <cristimarronaldo@gmail.com>
2017-09-21 07:03:15 -0300

    angular2-helloworld
    estudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { SegundoExemploComponent} from './segundo-exemplo/segundo-exemplo.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    SegundoExemploComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/segundo-exemplo/segundo-exemplo.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'segundo-exemplo-component',
+    template: `
+    <p>segundo component com Angular 2!</p>
+    <h1> component com Angular 2!</h1>
+    `
+})
+
+export class SegundoExemploComponent{}
