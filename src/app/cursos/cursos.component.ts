676a73708de7422247dd5b922a5f34677db7ee21
Cristimarronaldo <cristimarronaldo@gmail.com>
2017-09-21 08:59:47 -0300

    angular com for

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java','.Net','Angular']
+
+  constructor() { 
+    this.nomePortal = 'http://www.google.com.br'
+  }
 
   ngOnInit() {
   }
